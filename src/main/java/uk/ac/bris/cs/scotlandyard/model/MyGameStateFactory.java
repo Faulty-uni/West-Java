2be3caac2cf878373f0930dfd17d74840511c121
Oscar <rn24588@bristol.ac.uk>
2025-02-21 02:08:55 +0000

    altered MyGameStateFactory to include constructor

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -1,11 +1,14 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
-
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import java.util.*;
 import javax.annotation.Nonnull;
-
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
-import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+import uk.ac.bris.cs.scotlandyard.model.Move.*;
+import uk.ac.bris.cs.scotlandyard.model.Piece.*;
+import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
 
 /**
  * cw-model
@@ -13,13 +16,37 @@ import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
  */
 public final class MyGameStateFactory implements Factory<GameState> {
 
-	@Nonnull @Override public GameState build(
+	private final class MyGameState implements GameState {
+
+
+		//final means that the data that it initialises is "immutable"
+		//like const except is initialised at runtime rather than compile time
+		private MyGameState(
+			final GameSetup setup,
+			final ImmutableSet<Piece> remaining,
+			final ImmutableList<LogEntry> log,
+			final Player mrX,
+			final List<Player> detectives){
+			
+			}
+		}
+		private GameSetup setup;
+		private ImmutableSet<Piece> remaining;
+		private ImmutableList<LogEntry> log;
+		private Player mrX;
+		private List<Player> detectives;
+		private ImmutableSet<Move> moves;
+		private ImmutableSet<Piece> winner;
+		@Override public GameSetup getSetup() { return null;}
+		@Override public ImmutableSet<Piece> getPlayers() {return null;}
+		@Override public GameState advance(Move move) {return null;}
+	}
+	/*@Nonnull @Override public GameState build(
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
 		// TODO
 		throw new RuntimeException("Implement me!");
-
-	}
+	*/
 
 }
