e2e5c877885217bcf19cf79973d3dcd8b2fb55c7
Oscar <rn24588@bristol.ac.uk>
2025-02-26 10:53:36 +0000

    added more to constructor to pass tests

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -16,7 +16,7 @@ import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
  */
 public final class MyGameStateFactory implements Factory<GameState> {
 
-	private final class MyGameState implements GameState {
+	private final class MyGameState implements GameState, Board.TicketBoard {
 
 		private GameSetup setup;
 		private ImmutableSet<Piece> remaining;
@@ -25,6 +25,7 @@ public final class MyGameStateFactory implements Factory<GameState> {
 		private List<Player> detectives;
 		private ImmutableSet<Move> moves;
 		private ImmutableSet<Piece> winner;
+//		private int numOfX;
 
 		//final means that the data that it initialises is "immutable"
 		//like const except is initialised at runtime rather than compile time
@@ -35,16 +36,40 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			final Player mrX,
 			final List<Player> detectives){
 
+//			this.numOfX = 0;
 			this.setup = setup;
 			this.remaining = remaining;
 			this.log = log;
 			this.mrX = mrX;
 			this.detectives = detectives;
 
-			if(setup.moves.isEmpty()) throw new IllegalArgumentException("Moves is empty");
+			if (setup.moves.isEmpty()) throw new IllegalArgumentException("Moves is empty");
+			if (this.detectives.isEmpty()) throw new IllegalArgumentException("Detectives is empty");
+			if (!this.mrX.isMrX()) throw new IllegalArgumentException("MrX is null");
+
+			
+			/*			for (Player detective : detectives) {
+				if (getLocation(detective) != ) throw new IllegalArgumentException("2 Detectives in the same place");
+			}
+			for (Piece p : this.remaining) {
+				System.out.println(numOfX);
+				if (p.isDetective()) numOfX+=1;
+				if (numOfX > 1) throw new IllegalArgumentException("MrX contains more than one piece");
+			}*/
+
 		}
 		@Override public GameSetup getSetup() { return setup;}
 		@Override public ImmutableList<LogEntry> getMrXTravelLog() { return log; }
+
+		@Nonnull @Override public ImmutableSet<Piece> getWinner() {
+
+			return null;
+		}
+
+		@Nonnull @Override public ImmutableSet<Move> getAvailableMoves() {
+			return null;
+		}
+
 		@Override public Optional<Integer> getDetectiveLocation(Detective detective) {
 			//loop over detectives, if the piece is detective return location in Optional.of()
 
@@ -53,12 +78,17 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			}
 			return Optional.empty();
 		}
-		@Nonnull Optional<Board.TicketBoard> getPlayerTicket(Piece piece) {
-			if (piece == detectives) return Optional.of(1)
-			return Optional.empty();
+		@Nonnull public Optional<Board.TicketBoard> getPlayerTickets(Piece piece) {
+			if (piece == detectives) return Optional.empty();
+			return Optional.of(this::getCount);
 		}
 		@Override public ImmutableSet<Piece> getPlayers() {return null;}
 		@Override public GameState advance(Move move) {return null;}
+
+		@Override
+		public int getCount(@Nonnull Ticket ticket) {
+			return log.size();
+		}
 	}
 	@Nonnull @Override public GameState build(
 			GameSetup setup, Player mrX, ImmutableList<Player> detectives) {
