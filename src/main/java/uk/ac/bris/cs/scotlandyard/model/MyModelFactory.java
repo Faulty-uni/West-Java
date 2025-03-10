87318d4e77f91b86e83ad5919e496c9c142fafeb
Oscar <rn24588@bristol.ac.uk>
2025-03-10 22:29:16 +0000

    registering observer tests pass

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyModelFactory.java
@@ -1,13 +1,12 @@
 package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
-
-import javax.annotation.Nonnull;
-
 import com.google.common.collect.ImmutableSet;
+import javax.annotation.Nonnull;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -15,13 +14,12 @@ import java.util.Optional;
  * Stage 2: Complete this class
  */
 public final class MyModelFactory implements Factory<Model> {
-
-	ImmutableSet<Model.Observer> observers = ImmutableSet.of();
-
 	@Nonnull @Override public Model build(GameSetup setup,
 	                                      Player mrX,
 	                                      ImmutableList<Player> detectives) {
 		return new Model(){
+			ImmutableSet<Model.Observer> observerSet = ImmutableSet.of();
+			List<Observer> newObserverSet = new ArrayList<>();
 
 			@Nonnull @Override
 			public Board getCurrentBoard() {
@@ -31,25 +29,37 @@ public final class MyModelFactory implements Factory<Model> {
 			@Override
 			public void registerObserver(@Nonnull Observer observer) {
 				if (observer.equals(null)) throw new NullPointerException("Null observer");
-				//if (observers.contains(observer)) throw new IllegalArgumentException("Observer already registered");
-				for (Observer o : observers) {
+				for (Observer o : observerSet) {
 					if (o.equals(observer)) throw new IllegalArgumentException("Observer already registered");
 				}
-				observers.add(observer);
+				newObserverSet.addAll(observerSet);
+				newObserverSet.add(observer);
+				observerSet = ImmutableSet.copyOf(newObserverSet);
+				newObserverSet.clear();
 
+//				observers.stream()
+//				.filter(o -> o == observer)
+//				.findAny()
+//				.ifPresentOrElse(o -> {throw new IllegalArgumentException("Observer already registered");}, () -> observers.add(observer));
 			}
 
 			@Override
 			public void unregisterObserver(@Nonnull Observer observer) {
+				if (this.equals(null)) throw new IllegalArgumentException("Observers list is empty");
 				if (observer.equals(null)) throw new NullPointerException("Null observer");
-				if (observers.contains(observer)) observers.remove(observer);
-				throw new IllegalArgumentException("Observer not registered");
+				if (observerSet.contains(observer)) {
+					newObserverSet.addAll(observerSet);
+					newObserverSet.remove(observer);
+					observerSet = ImmutableSet.copyOf(newObserverSet);
+					newObserverSet.clear();
+				}
+				else throw new IllegalArgumentException("Observer not registered");
 
 			}
 
 			@Nonnull @Override
 			public ImmutableSet<Observer> getObservers() {
-				return Optional.ofNullable(observers).orElse(ImmutableSet.of());
+				return Optional.ofNullable(observerSet).orElse(ImmutableSet.of());
 			}
 
 			@Override
